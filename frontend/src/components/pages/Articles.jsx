fa2cc72918352b6728fb6ee0b0dc79dc05c486f4
magui165-byte <113001565+magui165-byte@users.noreply.github.com>
2025-05-15 23:43:44 -0400

    articles are displayed from the db in the articles page

--- frontend/src/components/pages/Articles.jsx
+++ frontend/src/components/pages/Articles.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { use } from 'react'
+import {useState, useEffect} from 'react'
+
+
 
 export const Articles = () => {
+
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+
+    getArticles();
+  },[])
+
+  const getArticles = async () => {
+    const url = 'http://localhost:3900/api/articles';
+    let request = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    let data = await request.json();
+
+    if(data.status === 'success'){
+      setArticles(data.articles);
+    }else{
+      console.log('Error in the request');
+    }
+  }
+
+
   return (
-    <div>Articles</div>
+    <>
+    {articles.map(article => {
+      return(
+        <article key={article.id} className="article-item">
+        <div className='mask'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="Portada" />
+        </div>
+        <div className="article-info">
+          <h3 className="title">{article.title}</h3>
+          <p className="description">{article.description}</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </div>
+      </article>
+      )
+    })}
+      
+    </>
   )
 }
--- frontend/src/components/pages/Main.jsx
+++ frontend/src/components/pages/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Main = () => {
   return (
-    <div>Main</div>
+    <div className='jumbo'>
+      <h1>Welcome to my blog made with React </h1>
+      <p>Blog developed with MERN stack (Mongo, Express, React, and NodeJS</p>
+      <Link to='/articles' className='btn'>See all articles</Link>
+      </div>
   )
 }
