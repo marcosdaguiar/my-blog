f77fc50bca9cd4a1711f2f87ca57e98253cb3356
magui165-byte <113001565+magui165-byte@users.noreply.github.com>
2025-05-22 13:23:20 -0400

    modified article style to match main

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className="footer">
-            &copy; Master en JavaScript ES12 y TypeScript
+            &copy; Marcos Aguiar Blog 2025
     </footer>
   )
 }
